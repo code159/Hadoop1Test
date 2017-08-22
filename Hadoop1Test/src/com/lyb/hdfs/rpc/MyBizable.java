750732b70b16979837c719197f92a3982b06fbf0
liyubin <lixin58688@163.com>
2017-08-22 13:53:29 +0800

    新增maven支持，增加org.apache.hadoop:hadoop-core:1.0.3、junit:junit:4.4依赖

--- Hadoop1Test/src/com/lyb/hdfs/rpc/MyBizable.java
+++ Hadoop1Test/src/com/lyb/hdfs/rpc/MyBizable.java
@@ -1,7 +1,11 @@
 package com.lyb.hdfs.rpc;
 
+import java.io.IOException;
+
 import org.apache.hadoop.ipc.VersionedProtocol;
 
 public interface MyBizable extends VersionedProtocol {
-	public abstract String hello(String name);
+	abstract String hello(String name);
+
+	long getProtocolVersion(String protocol, long clientVersion) throws IOException;
 }
